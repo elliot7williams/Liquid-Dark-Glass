5b30bcc896dbe4def116a84e568080396f7f454b
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:34:54 -0400

    Add Liquid Dark Glass/Liquid Dark Glass/Liquid_Dark_GlassApp.swift

---
+++ Liquid Dark Glass/Liquid Dark Glass/Liquid_Dark_GlassApp.swift
@@ -0,0 +1,17 @@
+//
+//  Liquid_Dark_GlassApp.swift
+//  Liquid Dark Glass
+//
+//  Created by Elliot Williams on 2025-07-18.
+//
+
+import SwiftUI
+
+@main
+struct Liquid_Dark_GlassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
